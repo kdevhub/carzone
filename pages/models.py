c4a98bfe24c59ae4dcd2b82225aaf9c580aa7fce
smile <webpagedeveloperkt@gmail.com>
2023-01-21 02:00:46 -0700

    Completed teams Model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    objects = None
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photo/%Y/%M/%d/')
+    facebook_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+    google_plus_link = models.URLField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
+
